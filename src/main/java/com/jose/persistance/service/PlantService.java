24d4419ee456ec654bc1b4c1d98123f2f15ee966
Jose Carlos Soto Morales <josecsmorales@gmail.com>
2020-09-26 21:47:29 -0300

    Added PlantRepository.

--- src/main/java/com/jose/persistance/service/PlantService.java
+++ src/main/java/com/jose/persistance/service/PlantService.java
@@ -1,13 +1,34 @@
 package com.jose.persistance.service;
 
 import com.jose.persistance.data.Plant;
+import com.jose.persistance.repository.PlantRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 @Service
 public class PlantService {
 
+    @Autowired
+    PlantRepository plantRepository;
+
     public Plant getPlantByName(String name){
         return new Plant();
+    }
+
+    public Long save(Plant plant){
+        return plantRepository.save(plant).getId();
+    }
+
+    public Boolean delivered(Long id){
+        // return plantRepository.deliveryCompleted(id);
+        return plantRepository.existsPlantByIdAndDeliveryCompleted(id, true);
+    }
+
+    public List<Plant> findPlantsBelowPrice(BigDecimal price) {
+        return plantRepository.findByPriceLessThan(price);
     }
 
 }
